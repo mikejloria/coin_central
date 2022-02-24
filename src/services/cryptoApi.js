bf576c2002b60a4d33d4038628b8470cfe1f7988
MLoria <mikejloria@gmail.com>
2022-02-23 21:35:38 -0800

    implemented redux stored state

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "0202e75fa8msh7e7122c9718c6e3p16542bjsnd1a89397c084",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
